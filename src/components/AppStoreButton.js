4b711d6c290253039c020362cff888fd3caa94d5
Marko Masnjak <marko@marko-mbp.lan>
2020-12-26 20:06:28 -0600

    Fix size of App Store button

--- src/components/AppStoreButton.js
+++ src/components/AppStoreButton.js
@@ -1,51 +1,23 @@
-import React, { useState } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import { OutboundLink } from "gatsby-plugin-google-analytics"
 
-const AppStoreButton = ({ href, invert }) => {
-  const [hasFocus, setHasFocus] = useState(false)
-  const color = invert ? "white" : "black"
-
-  const styles = {
-    root: {
-      display: "inline-block",
-      width: "14.4rem",
-      height: "4.8rem",
-      borderRadius: "0.6rem",
-      outline: "none",
-    },
-    image: {
-      display: "block",
-      width: "100%",
-      height: "auto",
-    },
-  }
-
-  const handleFocus = () => {
-    setHasFocus(!hasFocus)
-  }
-
-  return (
-    <OutboundLink
-      rel="noopener noreferrer"
-      style={styles.root}
-      href={href}
-      target="_blank"
-      onFocus={handleFocus}
-      onBlur={handleFocus}>
-      <img
-        style={styles.image}
-        width="120"
-        height="40"
-        src={`/app-store-download-${color}.svg`}
-        alt="Download from the App Store"
-      />
-    </OutboundLink>
-  )
-}
+const AppStoreButton = ({ href }) => (
+  <OutboundLink
+    rel="noopener noreferrer"
+    style={{ outline: "none" }}
+    href={href}
+    target="_blank">
+    <img
+      width="160"
+      height="54"
+      src={`/app-store-download-black.svg`}
+      alt="Download from the App Store"
+    />
+  </OutboundLink>
+)
 
 AppStoreButton.propTypes = {
-  invert: PropTypes.bool,
   href: PropTypes.string.isRequired,
 }
 
